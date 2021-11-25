f36f52a46789f982ddb6530ced02f7e56579294e
Filipe Campos Zeferino <filipezef@gmail.com>
2021-11-25 08:30:49 -0300

    edit and update actions and views created

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'galheta', to: 'pages#galheta'
-  resources :articles, only: [:show, :index, :new, :create]
+  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
 end
