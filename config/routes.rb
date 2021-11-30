df6752f5fe3e5bea37bb1d43c225475276aa8848
Filipe Campos Zeferino <filipezef@gmail.com>
2021-11-30 12:10:41 -0300

    article resource finished

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
   get 'galheta', to: 'pages#galheta'
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
