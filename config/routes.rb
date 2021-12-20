6aaf0b57ff51744c61266d8f08d3372bb476ecac
Filipe Campos Zeferino <filipezef@gmail.com>
2021-12-20 08:37:01 -0300

    image removed from about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#index'
   get 'about', to: 'pages#about'
-  get 'new', to: 'pages#new' 
+  get 'new', to: 'pages#new'
   get 'back-end', to: 'pages#back_end'
   get 'front-end', to: 'pages#front_end'
   resources :articles
