62d2d271b051faa4db0435ea128dcde4c69722bb
Filipe Campos Zeferino <filipezef@gmail.com>
2022-02-28 19:08:14 -0300

    update routes and customs.css.scss

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  ########## Root ##########
   root 'pages#about'
+  ########## Pages ##########
   get 'back-end', to: 'pages#back_end'
   get 'front-end', to: 'pages#front_end'
+  get 'home', to: 'pages#home'
+
+  ########## Articles ##########
   resources :articles
-  get 'signup', to: 'users#new'
+  
+  ########## Users ##########
   resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+  
+  ########## Sessions ##########
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+
+  ########## Categories ##########
   resources :categories, except: [:destroy]
 end
