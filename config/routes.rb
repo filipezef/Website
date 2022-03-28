f8c473de3b488c6b551da5e2373a782521c3cb84
Filipe Campos Zeferino <filipezef@gmail.com>
2022-03-28 07:56:30 -0300

    pages routes removed, root pointing to pages controller home action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   ########## Root ##########
   root 'pages#home'
-  ########## Pages ##########
-  get 'back-end', to: 'pages#back_end'
-  get 'front-end', to: 'pages#front_end'
-  get 'home', to: 'pages#home'
 
   ########## Articles ##########
   resources :articles
